de599d2c366589b1ed1fb0157c1bb90e1fff35b6
Azizul Haque Alif <55746705+azizulhaquealif@users.noreply.github.com>
2021-08-08 10:38:37 +0600

    family-fb-0

---
+++ family-fb-backend/src/main/java/com/example/familyfb/FamilyFbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.familyfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFbBackendApplication.class, args);
+	}
+
+}
---
+++ family-fb-backend/src/test/java/com/example/familyfb/FamilyFbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familyfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
